823604f6bfd7151a3a3c05fb85875ba87f0ce48d
ravi <ravi@DESKTOP-5E1JI8U>
2018-07-15 14:15:27 +0530

    Initiated project

---
+++ Spring-Boot-Security-login-InmemoryDB/src/main/java/com/ravi/security/SpringBootSecurityLoginInmemoryDbApplication.java
@@ -0,0 +1,12 @@
+package com.ravi.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityLoginInmemoryDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityLoginInmemoryDbApplication.class, args);
+	}
+}
---
+++ Spring-Boot-Security-login-InmemoryDB/src/test/java/com/ravi/security/SpringBootSecurityLoginInmemoryDbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ravi.security;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootSecurityLoginInmemoryDbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
